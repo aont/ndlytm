c03c39dd1f3ae3b963e6e3417d828835a1b0af4d
aont <220471+aont@users.noreply.github.com>
2026-02-18 13:45:53 +0900

    Update bookmarklet.js

--- bookmarklet.js
+++ bookmarklet.js
@@ -1,3 +1,5 @@
-const w = document.getElementById("HTML5PlayerFrame")?.contentWindow || window
-const url = "https://aont.github.io/ndl-webui/#?jsonInput=" + encodeURIComponent(JSON.stringify({"Cookie":document.cookie,"BaseURL":window.location.origin,"PlayListsTracks":w.PlayListsTracks}))
-window.open(url);
+(function(){
+  const w = document.getElementById("HTML5PlayerFrame")?.contentWindow || window
+  const url = "https://aont.github.io/ndlytm/#?jsonInput=" + encodeURIComponent(JSON.stringify({"Cookie":document.cookie,"BaseURL":window.location.origin,"PlayListsTracks":w.PlayListsTracks}))
+  window.open(url);
+})();
