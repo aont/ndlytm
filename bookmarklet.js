a7a4af70c5c22341966f2d0aa7632f2956b25e6b
aont <220471+aont@users.noreply.github.com>
2026-02-18 14:30:42 +0900

    Update bookmarklet.js

--- bookmarklet.js
+++ bookmarklet.js
@@ -1,5 +1,14 @@
 (function(){
   const w = document.getElementById("HTML5PlayerFrame")?.contentWindow || window
-  const url = "https://aont.github.io/ndlytm/#?jsonInput=" + encodeURIComponent(JSON.stringify({"Cookie":document.cookie,"BaseURL":window.location.origin,"PlayListsTracks":w.PlayListsTracks}))
-  window.open(url);
+  const url = "https://aont.github.io/ndlytm/#?jsonInput=" + encodeURIComponent(JSON.stringify({
+    "Cookie": document.cookie,
+    "BaseURL": window.location.origin,
+    "PlayListsTracks":w.PlayListsTracks,
+    "AlbumArt": w.document.querySelector("#album-link > img").src
+  }))
+  const anchor = document.createElement('a');
+  anchor.href = url;
+  anchor.target = '_blank';
+  anchor.click();
+  anchor.remove()
 })();
